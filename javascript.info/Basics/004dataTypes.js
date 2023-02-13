725bfb82adcda9b7e93cc146090c58782cf6d623
TrinayBhati <bhatiji780780@gmail.com>
2023-02-13 11:16:07 +0530

    13-feb

---
+++ javascript.info/Basics/004dataTypes.js
@@ -0,0 +1,12 @@
+//8 basic types of data types
+
+var message = "hello";
+message = 123456;
+
+// can initilize variable with anything n turn it to anything these types of programming languages are called "dynamically typed"
+
+var number = 123;
+number = 1.123;
+
+
+
