60c4f1d66609c327a8edf51a26021423d5f3122d
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-05 21:02:06 +0530

    5-march

---
+++ javascript.info/objects/1objects.js
@@ -0,0 +1,10 @@
+	// key value pair
+	// let user = new Object(); object construct we can declare an object like this
+	let user  ={ // an object
+		name : "John", // key is the name and value is John
+		age =30, //  key is agre and value is 30
+
+	};
+	alert(user.name); // John
+	let key = prompt("What would you like to know about user","");
+	alert(user[key]);
---
+++ yt-tutorial/js-course-clever-progammer/yourPlayground.js
@@ -0,0 +1,4 @@
+// let rem = Math.floor(22.25);
+// let cel = Math.ceil(22.25);
+// console.log(rem);
+// 
