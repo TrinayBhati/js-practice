8a0ae8d61dbe158c41cf25e9e11cd77189364220
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-09 23:44:41 +0530

    9-march

--- yt-tutorial/js-course-clever-progammer/yourPlayground.js
+++ yt-tutorial/js-course-clever-progammer/yourPlayground.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	
+
 	// 	function calculateFoodTotal(food, tip) {
    	// 	const tipPercentage = tip / 100
    	// 	const tipAmount = food * tipPercentage
@@ -15,28 +14,121 @@
  	// groceries.slice(2,4);
  	// groceries.length;
 
- 	let person = (name, age, pay, expenses)=>{
- 	let pop = {
- 		pay: pay,
- 		expenses : expenses,
- 		 savings: function () {
- 			return this.pay - this.expenses;
- 		}
+ 	// let person = (name, age, pay, expenses)=>{
+ 	// let pop = {
+ 	// 	pay: pay,
+ 	// 	expenses : expenses,
+ 	// 	 savings: function () {
+ 	// 		return this.pay - this.expenses;
+ 	// 	}
  		
- 	};
- 	let intro = `My name is ${name} and I'm ${age} years old. my savings are ${pop.savings()}`
- 	return intro;
- }
+ 	// };
+ 	// let intro = `My name is ${name} and I'm ${age} years old. my savings are ${pop.savings()}`
+ 	// return intro;
+ 	// }
 
- 	console.log(person("Trinay", 22, 2000, 500));
- 	const numbers= [2,3,4,5,2,6,2,7,9];
- 	for(let number of numbers){
- 		result.push(number*2)
- 	}
+ 	// console.log(person("Trinay", 22, 2000, 500));
+ 	// const numbers= [2,3,4,5,2,6,2,7,9];
+ 	// for(let number of numbers){
+ 	// 	result.push(number*2)
+ 	// }
+	
+	// const sum = (numbers)=>{
+	// 	let result = 0;
+	// 	for(let number of numbers){
+	// 		result = result + number;
+	// 	}
+	// 	return result;
+	// }	
 
+ 	// const max = (numbers)=>{
+ 	// 	let max = numbers[0];
+ 	// 	for(let number of numbers){
+ 	// 		if(number > max){
+ 	// 			max = number;
+ 	// 		}
+ 	// 		return max;
+ 	// 	}
+ 	// }
+	
+	// const frequency = (phrase)=>{
+	// 	let counter = 0;
+	// 	for(int i = 0; i<phrase.length; i++){
+	// 		for(int j = 1; j<phrase.length; j++){
+	// 			if(phrase[i] == phrase[j]){
+	// 				counter++;
+	// 			}
+	// 		}
+	// 		return counter; 
+	// 	}
+	// } 
+	// const frequncyCounter = (array)=>{
+	// 	let frequency = {}
+	// 	for(let element of array){
+	// 		if(element in frequency){
+	// 			frequency[element]= frequeny[letter]+1;
+	// 		} 
+	// 		else{
+	// 			frequency[letter]=1;
+	// 		}
+	// 	}
+	// 	return frequency;
+	// }	
 
+	// const wordFrequecy = (phrase) =>{
+	// 	let frequency= {}
+	// 	let array = phrase.split(' ');
+	// 	for(let word of array){
+	// 		if(word in frequency){
+	// 			frequency[word] = frequency[word]+1;
+	// 		}
+	// 		else{
+	// 			frequency[word] = 1;
+	// 		}
+	// 	} 
+	// 	console.log(word);
+	// 	return frequency;
+	// }
 
+	// const hourToSec = (hours) =>{
+	// 	let seconds = hours * 360;
+	// 	return seconds;
+	// }
 
+	// [1,2,3,4,5,6].map(number =>{
+	// 	console.log(number);
+	// 	console.log("yo");
+	// })
+	
+	// let double = [2123,23,442,55].map(number => number*2);
+	// console.log(double);
+	
+	// const doubleFunction = (number) =>{
+	// 	return number.map(number =>number*2)
+	// }
+	
+	// cont filter = (numbers , greaterThan) =>{
+	// 	let arr = [];
+	// 	for(let number of numbers){
+	// 		if(number > greaterThan){
+	// 			arr.push(number)
+	// 		}
+	// 	}
+	// 	return arr;
+	// } 
 
+	// const nums = [1,2,3,4,5,6,7,];
+	// console.log(nums.filter(num => num >3));
+	
+	// const actors = [
+	// 	{name : Hritik , worth:20}
+	// 	{name : salman , worth : 30}
+	// 	{name: sharukh  ,worth :40}
+	// 	]
+	// let top = actors.filter(actor => actor.worth>20)
+	
+	// .join gives string from array, unlike splice which gives array from strings
+	// reduce akes a function as argumanet
 
-</script>
+	
+
