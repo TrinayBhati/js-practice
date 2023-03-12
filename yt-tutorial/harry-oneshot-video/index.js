4664fbc17eaf83ae1d7d81bd41c42d020d45760d
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-12 23:59:48 +0530

    12-march

--- yt-tutorial/harry-oneshot-video/index.js
+++ yt-tutorial/harry-oneshot-video/index.js
@@ -277,7 +277,7 @@ logKaro = ()=>{
     document.querySelectorAll('.container')[1].innerHTML = "<b> Set interval fired</b>"
     console.log("I am your log")
 }
-// SetTimeout and setinterval
+// SetTimeout and setinterval 
 // clr = setTimeout(logKaro, 5000);
 // clr = setInterval(logKaro, 2000);
 // use clearInterval(clr)/clearTimeout(clr) to cancel setInterval/setTimeout
