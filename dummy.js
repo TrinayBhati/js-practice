3fba276c0ba70ea5c1c48db0a91d7d21ab576311
Trinay Bhati <bhatiji780780@gmail.com>
2023-04-13 23:41:17 +0530

    13-apr

--- dummy.js
+++ dummy.js
@@ -1,10 +1,217 @@
+//const users = ["Alice", "Dan", "Jessica"];
+          // const query = "A";
+
+          // const user = users.filter((item) => item.startsWith(query));
+
+          // console.log(user);
+
+          // // Function has a closure over a variable
+          // // or
+          // // A function closes over a variable
+
+          // /************************************************/
+
+          // // Lexical Scope
+
+          // function outermost() {
+          //   const user = "Deadpool";
+          //   function inner() {
+          //     function innerMost() {
+          //       const score = 100;
+          //     }
+          //   }
+          // }
+
+          // /************************************************/
+
+          // // Garbage Collector
+          // function liveAday() {
+          //   const food = "cheese";
+          //   const one = 1;
+          //   const two = 2;
+          //   function eat() {
+          //     console.log("Eating...", food);
+          //   }
+          //   return eat;
+          // }
+
+          // const innerFunction = liveAday();
+
+          // /************************************************/
+
+
+          // // Memory Efficiency
+          // // function heavyDuty(index) {
+          // //   const bigArray = new Array(7000).fill("🔥");
+          // //   console.log("Array Created");
+          // //   return bigArray[index];
+          // // }
+
+          // // console.log(heavyDuty(700));
+          // // console.log(heavyDuty(100));
+          // // console.log(heavyDuty(100));
+          // // console.log(heavyDuty(100));
+          // // console.log(heavyDuty(100));
+          // // console.log(heavyDuty(100));
+
+          // function heavyDuty2() {
+          //   const bigArray = new Array(7000).fill("🔥");
+          //   console.log("Array Created");
+          //   return function (index) {
+          //     return bigArray[index];
+          //   };
+          // }
+
+          // const getHeavyDuty = heavyDuty2();
+          // getHeavyDuty(1);
+          // getHeavyDuty(87);
+          // getHeavyDuty(99);
+
+          // /************************************************/
+
+          // // Closures
+
+          // // Encapsulation & Abstraction
+
+          // const rahulSalary = (function () {
+          //   let salary = 60000;
+
+          //   function changeBy(amount) {
+          //     salary += amount;
+          //   }
+
+          //   return {
+          //     raise: function () {
+          //       changeBy(5000);
+          //     },
+          //     lower: function () {
+          //       changeBy(-10000);
+          //     },
+          //     currentAmount: function () {
+          //       return salary;
+          //     },
+          //   };
+          // })();
+
+          // console.log(rahulSalary.currentAmount());
+          // rahulSalary.raise();
+          // console.log(rahulSalary.currentAmount());
+          // rahulSalary.lower();
+          // console.log(rahulSalary.currentAmount());
+
+          // // Callbacks
+          // function getVideoDetails(videos, callback) {
+          //   // Network Call to the backend with videos
+          //   setTimeout(() => {
+          //     callback({ id: 1, title: "Hey Jack!", duration: 10 });
+          //   }, 1500);
+          // }
+
+          // function getUserVideos(email, callback, errorCallback) {
+          //   // Network Call to the backend with email
+          //   setTimeout(() => {
+          //     callback(["video1", "video2", "video3"]);
+          //   }, 1500);
+          // }
+
+          // function loginUser(email, password, successCallback, errorCallback) {
+          //   // Network Call to the backend with email and password
+          //   setTimeout(() => {
+          //     let error = true;
+          //     if (error) {
+          //       errorCallback("Network call Unsuccessful!");
+          //     } else {
+          //       success({ userEmail: email });
+          //     }
+          //   }, 1500);
+          // }
+
+          // loginUser(
+          //   "david@gmail.com",
+          //   12345,
+          //   (userObject) => {
+          //     getUserVideos(
+          //       userObject.email,
+          //       (videos) => {
+          //         getVideoDetails(
+          //           videos,
+          //           () => {
+          //             console.log(videos);
+          //           },
+          //           () => {
+          //             console.log("Error");
+          //           }
+          //         );
+          //       },
+          //       (error) => {
+          //         console.log("Error");
+          //       }
+          //     );
+          //   },
+          //   (error) => {
+          //     console.log("Error");
+          //   }
+          // );
+
+
+          // let user = {
+
+          // firstName: 'naman',
+
+          // lastName: 'jindal',
+
+          // age: 10,
+
+          // courseList: [],
+
+          // /* key value itself */
+
+          // /*
+
+          // arrow functions do not have their own binding of this ->
+
+          // if we are usign arrow functions in that case it matters where the function defination
+
+          // exists, we will be going up the heirarchy to find the parent having its own this and
+
+          // that will act as the value of this for that arrow function
+
+          // whereas in the case of normal functions it matter how that function is getting called.
+
+          // */
+
+          // buyCourse: (courseName) =>{
+
+          // console.log(this) //window
+
+          // this.courseList.push(courseName);
+
+          // },
+
+          // getCourse: function(){
+
+          // //console.log(this) ->
+
+          // console.log(this.courseList)
+
+          // }
+
+          // }
+
+          // user.buyCourse('react'); //
+
+          // user.getCourse(); //
+
+
+//============= practice =====================
+
 // normal function
 // functions are first clas citizens
 // function speak (){
 //   console.log("speaking");
 // }
 // speak();
- 
+
 // arrow function
 // let speak = () =>{
 //   console.log("speaking");
@@ -72,18 +279,18 @@
 // object.speak();
 
 // let randomColor = () =>{
-//      let r = Math.floor(Math.random()*255);
-//      let g = Math.floor(Math.random()*255);
-//      let b = math.floor(math.random()*255);
-//    }
-//    randomColor();
+//          let r = Math.floor(Math.random()*255);
+//          let g = Math.floor(Math.random()*255);
+//          let b = math.floor(math.random()*255);
+//      }
+//      randomColor();
 
 //event propagation
 // <div id="grandParent">
-//    <div id="parent">
-//      <div id="child">
+//      <div id="parent">
+//          <div id="child">
+//          </div>
 //      </div>
-//    </div>
 //  </div>
 // event bubbling - bubble up - child parent grandParent
 // event capture - capture up - opposite of bubbling - grandParent parent child
@@ -226,7 +433,7 @@
 //  name : "lkhj",
 //  age:22,
 //  yo(){
-//    console.log(`waddup ${this.name}`);
+//      console.log(`waddup ${this.name}`);
 //  },
 // }
 // person.yo();
@@ -240,11 +447,11 @@
 //  return person; 
 // //generic method below
 // //   let person = {
-// //     name : name,
-// //     age : age,
-// //     work : function(){
-// //       console.log(`hello ${this.name}`);
-// //     }
+// //       name : name,
+// //       age : age,
+// //       work : function(){
+// //           console.log(`hello ${this.name}`);
+// //       }
 // //   }
 // //   return person;
 // }
@@ -253,38 +460,38 @@
 // personOne.work();
 
 // let sharedMethods = {
-//    work : function () {
-//      console.log(`hello ${this.name}`);
+//      work : function () {
+//          console.log(`hello ${this.name}`);
 //     }
 // }
 // function Person(name, age) {
 
-//    let person = {}
-//    person.name = name;
-//    person.age = age;
-//    person.work = sharedMethods.work;
-//    return person;
+//      let person = {}
+//      person.name = name;
+//      person.age = age;
+//      person.work = sharedMethods.work;
+//      return person;
 
-//    //generic method
+//      //generic method
 
-//    // let person = {
-//    //  name : name,
-//    // age : age,
-//    // work : sharedMethods.work,
-//    // }
-//    // return person;
+//      // let person = {
+//      //  name : name,
+//      // age : age,
+//      // work : sharedMethods.work,
+//      // }
+//      // return person;
 //  }
 //  let personOne = Person("Papu",22);
 //  personOne.work();
 
 
 // let Parent = {
-//    name : "idk",
-//    age : 12,
+//      name : "idk",
+//      age : 12,
 //   //work(){  console.log(` ${this}.name is working`),}
-//    work : function(){
-//      console.log(`${this}.name is working`),
-//    },
+//      work : function(){
+//          console.log(`${this}.name is working`),
+//      },
 //  };
 //  let child = Object.create(Parent);  // creates a new empty object
 // // delegates failed look ups to parent
@@ -373,246 +580,158 @@
 // }
 // getResult( "paper","rock");
 
-// promise is an object
-// have 3 states -> pending, fullfilled, rejected
-// pending intitial stage
-// pending can change from fullfilled or rejected
+//promises
+// // promise is an object
+// // have 3 states -> pending, fullfilled, rejected
+// // pending intitial stage
+// // pending can change from fullfilled or rejected
+// // why we need promises?
+// // to avoid callback hell
+// // when we use new on a constructor function it retuirns to us an object
+// let promise = new Promise((resolve, reject)=>{
+//   //asunchronous code
+//   //setTimeout
+//   //Network calls
+//   setTimeout(()=>{
+//     let noodlesAvailable = false;
+//     if(noodlesAvailable){
+//       resolve("Noodles recieved"); // pending -> fullfill
+//     }
+//     else{
+//       reject("Noodles not available");// pending -> rejected 
+//     }
+//   },3000);
+// });
+//       // console.log(promise);
+//       // setTimeout(()=>{
+//       //   console.log(promise);
+//       // },4000);
+// // if the promise is successfull then we run this 
+// // .then is a method on promise
+// promise.then((message)=>{
+//   console.log("setting table", message);
+// }).catch((message)=>{ // catch if the promise is not true unsuccessful 
+//   console.log("prepare maggi", message);
+// })
 
+//fetch
+//used for network and api calls
+// https://jsonplaceholder.typicode.com/todos/1
+// JSON -> javascript object notation
+  // fetch("https://jsonplaceholder.typicode.com/todos/1").then(response =>{
+  //   let full = response.json()
+  //   full.then((data)=>{
+  //     console.log(data);
+  //   });
+  // });
 
+//fetch("https://api.example.com/data").then(response =>)
+// fetch("https://jsonplaceholder.typicode.com/todos/5").then(response =>{
+//   return response.json();
+// }).then((data)=>{
+//   console.log(data);
+// });
 
+//async and await
+  // async function stuff(){ // an async function always gives us a promise wrapping the returned thing
+  //   return 2;
+  // }
+  // let result = stuff();
+  // result; // in this case it is a fullfilled promise, bc when you return a value it's fulfilled resolved promise already
+// async function arr(){
+//   return [1,2,3,4,5];
+// }
+// let promise = arr();
+// promise.then((data)=>{
+//   console.log(data);
+// });
+// async function stuff(){
+//   // return Promise.resolve("Success");
+//    return Promise.reject("Failed");
+// }
+// let promise = stuff();
+// promise.then((data)=>{
+//   console.log(data);
+// }).catch((error)=>{
+//   console.log("error");
+// }); 
 
-
-
-
-
-
-
-
-
-
-const users = ["Alice", "Dan", "Jessica"];
-const query = "A";
-
-const user = users.filter((item) => item.startsWith(query));
-
-console.log(user);
-
-// Function has a closure over a variable
-// or
-// A function closes over a variable
-
-/************************************************/
-
-// Lexical Scope
-
-function outermost() {
-  const user = "Deadpool";
-  function inner() {
-    function innerMost() {
-      const score = 100;
-    }
-  }
-}
-
-/************************************************/
-
-// Garbage Collector
-function liveAday() {
-  const food = "cheese";
-  const one = 1;
-  const two = 2;
-  function eat() {
-    console.log("Eating...", food);
-  }
-  return eat;
-}
-
-const innerFunction = liveAday();
-
-/************************************************/
-
-
-// Memory Efficiency
-// function heavyDuty(index) {
-//   const bigArray = new Array(7000).fill("🔥");
-//   console.log("Array Created");
-//   return bigArray[index];
+// async function getTodo(){
+//   // fetch("https://jsonplaceholder.typicode.com/todos/5").then((response)=>{
+//   //   response.json()
+//   // .then((data)=>{
+//   //   console.log(data);
+//   // });
+//   // });
+  
+//   // now we can use await keyword
+//   let rresponse = await fetch("https://jsonplaceholder.typicode.com/todos/5");
+//   let data = await response.json();
+// }
+// getTodo();
+// await returns the resolved value of the fulfilled function
+// async function stuff(){
+//   try {
+//     console.log("LFG");
+//   }
+//   catch(){
+//   }
 // }
 
-// console.log(heavyDuty(700));
-// console.log(heavyDuty(100));
-// console.log(heavyDuty(100));
-// console.log(heavyDuty(100));
-// console.log(heavyDuty(100));
-// console.log(heavyDuty(100));
+//error handling in fetch
+  // async function todo(){
+  //   try{
+  //     let response = await fetch("https://jsonplaceholder.typicode.com/todos/5");
+  //   // console.log(response);
+  //     if(!response.ok){
+  //       let message = "error";
+  //       throw new Error(message);
+  //     }
+  //   let data = await response.json();
+  //   console.log(data);
+  //   }
+  //   catch(error){
+  //     console.log(message);
+  //   }
+  // }
+  // todo();
+  
 
-function heavyDuty2() {
-  const bigArray = new Array(7000).fill("🔥");
-  console.log("Array Created");
-  return function (index) {
-    return bigArray[index];
-  };
-}
+// function checkCanIVote( delay , age) {
+//   let promise = new Promise((resolve, reject)=>{
+//     if(age>18){
+//       setTimeout(()=>{
+//         resolve("You can vote")
+//       }, delay);
+//     }
+//     else{
+//       setTimeout(()=>{
+//         resolve("You can not vote")
+//       }, delay);
+//     }
+//   })
+//     return promise;
 
-const getHeavyDuty = heavyDuty2();
-getHeavyDuty(1);
-getHeavyDuty(87);
-getHeavyDuty(99);
+// }
+// class Account {
+//     static accounts = 0;
+//     static totalBalance = 0;
+//    constructor(accountNum, balance) {
+//      this.accountNum = accountNum;
+//      this.balance = balance;
+//      Account.accounts ++;
+//      Account.totalBalance += balance;
+//     }
+//     static returnTotalAccounts(){
+//       return Account.accounts;
+//     }
+//     static returnTotalBalance(){
+//       return Account.totalBalance;
+//     }
+// }
+// let newAccount = new Account("a112", 1000);
+// Account.returnTotalAccounts();
+// console.log(Account.returnTotalAccounts());
+// console.log(Account.returnTotalBalance());
 
-/************************************************/
-
-// Closures
-
-// Encapsulation & Abstraction
-
-const rahulSalary = (function () {
-  let salary = 60000;
-
-  function changeBy(amount) {
-    salary += amount;
-  }
-
-  return {
-    raise: function () {
-      changeBy(5000);
-    },
-    lower: function () {
-      changeBy(-10000);
-    },
-    currentAmount: function () {
-      return salary;
-    },
-  };
-})();
-
-console.log(rahulSalary.currentAmount());
-rahulSalary.raise();
-console.log(rahulSalary.currentAmount());
-rahulSalary.lower();
-console.log(rahulSalary.currentAmount());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Callbacks
-function getVideoDetails(videos, callback) {
-  // Network Call to the backend with videos
-  setTimeout(() => {
-    callback({ id: 1, title: "Hey Jack!", duration: 10 });
-  }, 1500);
-}
-
-function getUserVideos(email, callback, errorCallback) {
-  // Network Call to the backend with email
-  setTimeout(() => {
-    callback(["video1", "video2", "video3"]);
-  }, 1500);
-}
-
-function loginUser(email, password, successCallback, errorCallback) {
-  // Network Call to the backend with email and password
-  setTimeout(() => {
-    let error = true;
-    if (error) {
-      errorCallback("Network call Unsuccessful!");
-    } else {
-      success({ userEmail: email });
-    }
-  }, 1500);
-}
-
-loginUser(
-  "david@gmail.com",
-  12345,
-  (userObject) => {
-    getUserVideos(
-      userObject.email,
-      (videos) => {
-        getVideoDetails(
-          videos,
-          () => {
-            console.log(videos);
-          },
-          () => {
-            console.log("Error");
-          }
-        );
-      },
-      (error) => {
-        console.log("Error");
-      }
-    );
-  },
-  (error) => {
-    console.log("Error");
-  }
-);
-
-
-
-
-
-
-
-
-
-
-let user = {
-
-firstName: 'naman',
-
-lastName: 'jindal',
-
-age: 10,
-
-courseList: [],
-
-/* key value itself */
-
-/*
-
-arrow functions do not have their own binding of this ->
-
-if we are usign arrow functions in that case it matters where the function defination
-
-exists, we will be going up the heirarchy to find the parent having its own this and
-
-that will act as the value of this for that arrow function
-
-whereas in the case of normal functions it matter how that function is getting called.
-
-*/
-
-buyCourse: (courseName) =>{
-
-console.log(this) //window
-
-this.courseList.push(courseName);
-
-},
-
-getCourse: function(){
-
-//console.log(this) ->
-
-console.log(this.courseList)
-
-}
-
-}
-
-user.buyCourse('react'); //
-
-user.getCourse(); //
+// type= module in script makes it importable and exportable
