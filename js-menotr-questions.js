b59ca2bb3badecc48c28becdd6bd85de4838a1fc
Trinay Bhati <bhatiji780780@gmail.com>
2023-05-16 22:36:17 +0530

    16-may

--- js-menotr-questions.js
+++ js-menotr-questions.js
@@ -88,3 +88,115 @@
 // const obj1 = {name: 'Sachin'};
 // const obj2 = {name: 'Kohli'};
 // console.log({...obj1, ...obj2})
+
+// function real(){
+//   return "pussy";
+// }
+// function real(){
+//   return "tussy";
+// }
+// function real(){
+//   return "sussy";
+// }
+// real()
+
+// console.log("var", varName)
+// var varName;
+// console.log("var", varName)
+// varName = "Captain";
+// console.log("var", varName)
+
+// fn();
+// var fn = function(){
+//   return "hello";
+// }
+// fn();
+
+// var a = 10;
+// console.log("line 2" , a);//10
+// function fn (){
+//   console.log("line 4",a)//und
+//   var a = 20;
+//   a++;
+//   console.log("line 7",a);//21
+//   if(a){
+//     var a = 30;
+//     a++;
+//     console.log("line 11", a);//31
+//   }
+//   console.log("line 13", a);//21
+// }
+// fn();
+// console.log("line 16" , a);
+
+// console.log("1" + 2 + 3);
+
+// console.log(2 + 3 + "1");
+
+// const a = [1, 2, 3];
+// const b = [...a, 4, 5];
+// console.log(b);
+
+// const obj = { a: 1, b: 2 };
+// const { a, b } = obj;
+// console.log(a, b);
+
+// const x = 10;
+// const y = x--;
+// console.log(x, y);
+
+// const arr = [1, 2, 3];
+// const [x, ...y] = arr;
+// console.log(x, y);
+
+// console.log(typeof null);
+
+// const num = 10;
+// console.log(num.toString().length);
+
+// const arr = [1, 2, 3];
+// arr.forEach(function(num) {
+//   console.log(num * 2);
+// });
+
+// const a = { foo: "bar" };
+// const b = { foo: "bar" };
+// console.log(a === b);
+
+// const arr = [1, 2, 3];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// console.log(x);
+// var x = 5;
+
+// var x = 10;
+// function foo() {
+//   console.log(x);
+//   var x = 5;
+// }
+// foo();
+
+// function foo() {
+//   return bar();
+//   var bar = function() {
+//     return "Hello";
+//   };
+// }
+// console.log(foo());
+
+// function outer() {
+//   var x = 10;
+//   function inner() {
+//     console.log(x);
+//   }
+//   return inner;
+// }
+// var closure = outer();
+// closure();
+
+// console.log(typeof x);
+// console.log(typeof y);
+// var x = 10;
+// let y = 20;
+
