5988bc5a95a92ff947a80dbe757031b5c2123aae
Trinay Bhati <bhatiji780780@gmail.com>
2023-08-12 23:31:52 +0530

    12-aug

--- js-notes.js
+++ js-notes.js
@@ -348,4 +348,4 @@ const inviteEmployee1 = invite.bind(employee1);
 const inviteEmployee2 = invite.bind(employee2);
 inviteEmployee1("Hello", "How are you?");
 inviteEmployee2("Hello", "How are you?");
-==============================Destructuring==============================
+
