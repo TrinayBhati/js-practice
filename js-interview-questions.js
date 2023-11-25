3b060968c9079875a6dd21da445c7dca28a5059b
Trinay Bhati <bhatiji780780@gmail.com>
2023-11-25 23:31:18 +0530

    25-nov

---
+++ js-interview-questions.js
@@ -0,0 +1,75 @@
+//===================================interview 1============================================
+
+// how to make site responsive media queries
+// how to center element, position properties
+// includes indexof let var const
+// how to change key names in object
+// WHY WE USE JSON
+// array methods
+// object methods
+// large binary object
+// based on key how to define particular
+
+// split method
+// output based question 10
+// let ans = 10 + "20";
+// let ans = 10 + + "20";
+// console.log(ans, typeof (ans));
+// difference betwen array and object?
+// how to find a number in anarray?
+// how to find key in an object ?
+// all method knoweldge
+// if we want to remove first  element from an array
+// image tag attributes specially the one that we can use if image does not load instead of alt..
+// tell the attribute that defines or check image is there or not
+// margin or padding differnce ?? are they inclusive of parent tag or exclusive like if we have one div tag inside other
+// and aprent have 100px height and weidht and if we gce insdide one 20 px what happen in case of padding and margin
+//how can we put a tag in center except flex
+
+//inline  element and innline block element diff.
+//tell inline and block elelment
+//margin padding difference and padding is inclusivev parent height or exclusive
+// 100px h and w parent div and inside div ko 20px padding and margin dene p kta farak pdega?
+//100vw and 100vh what is this? and we want to center a div so tell how we can do that and also responsive? there is a prop by which we can make the div tag in center used with relative and abslute prop.? css m variable use kr skte h ya nhi?
+//what is calc?
+//can we use variables in css?
+
+// Example 1: This example uses window.innerHeight property to get the height of the device screen.The innerHeight property is used to return the height of the device.
+//     Output:
+// Example 2: This example uses document.documentElement. ...
+// Output:
+//data attribute
+
+//== and === difference
+// object and array differnece? homogenous nad heterogenous, key pair , indexes ,
+//find eleeent in an array tell the ways write find code? - includes, find, filter, indexOf
+//how to remove first tlement in an array tell methods? unshift from first, pop from last
+//[2,3,4,5] array remove 3 by finding it as assume uh dont know the index? -indexof se index nikala and then splice
+//object m se key value pair avaialble h ya nhi how ?? - forIn loop se check kia and console true krda by if condtion cehcek
+//key value pair delete kaise krte h?
+//how to change key and value both? means  name: trinay, naam: saurav
+//"i am trinay".split("").reverse().join("")
+//trim method
+
+//===================================interview 2============================================
+
+// api methods - backend
+// closures - what are uses
+// hoisting
+// block scope and function scope
+
+// console.log("A"-"B"+"2") //NaN
+// console.log("A"-"B"+2) //NaN
+
+// (function abc(a) {
+//   return (function () {
+//     console.log(a);
+//     a = 45;
+//   })();
+// })(22); // 22
+
+// const obj = {id : 1, name : "idk" , role : "frontend" }
+// convert keys into an array and same for values - using Object.keys and Object.values - check Object.entries
+
+// difficulty faced while making react
+// html tag used for pagination infinite scroll
